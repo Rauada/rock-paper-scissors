bd21d9ff634c564e2ed12c9904027b4e1a2b0e9d
Rafael Auada <auadarafael@gmail.com>
2022-02-08 20:53:45 -0300

    finished game UI

--- script.js
+++ script.js
@@ -12,65 +12,87 @@ function computerPlay(){
     return options[Math.floor(Math.random() * options.length)];
 }
 
-//get the player`s move
-function playerPlay() {
-    return window.prompt("Choose your move:", "rock, paper, scissors").toLowerCase();
-}
+// //get the player`s move
+// function playerPlay() {
+//     return window.prompt("Choose your move:", "rock, paper, scissors").toLowerCase();
+// }
 
 //plays a round of the game
 function playRound(playerSelection, computerSelection){
     computerSelection = computerPlay();
-    playerSelection = playerPlay();
 
     if (computerSelection === "rock" && playerSelection === "paper") {
         playerScore ++;
-        window.alert(`You Win! ${playerSelection} beats ${computerSelection}.`);
+        roundInfo.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`;
     }
     
     if (computerSelection === "rock" && playerSelection === "scissors") {
         computerScore ++;
-        window.alert(`You Lose! ${computerSelection} beats ${playerSelection}.`);
+        roundInfo.textContent = `You Lose! ${computerSelection} beats ${playerSelection}.`;
     }
 
     if (computerSelection === "rock" && playerSelection === "rock") {
-        window.alert("It`s a draw!");
+        roundInfo.textContent = "It's a draw!";
     }
 
     if (computerSelection === "paper" && playerSelection === "scissors") {
         playerScore ++;
-        window.alert(`You Win! ${playerSelection} beats ${computerSelection}.`);
+        roundInfo.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`;
     }
     
     if (computerSelection === "paper" && playerSelection === "rock") {
         computerScore ++;
-        window.alert(`You Lose! ${computerSelection} beats ${playerSelection}.`);
+        roundInfo.textContent = `You Lose! ${computerSelection} beats ${playerSelection}.`;
     }
 
     if (computerSelection === "paper" && playerSelection === "paper") {
-        window.alert("It`s a draw!");
+        roundInfo.textContent = "It's a draw!";
     }
 
     if (computerSelection === "scissors" && playerSelection === "rock") {
         playerScore ++;
-        window.alert(`You Win! ${playerSelection} beats ${computerSelection}.`);
+        roundInfo.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`;
     }
     
     if (computerSelection === "scissors" && playerSelection === "paper") {
         computerScore ++;
-        window.alert(`You Lose! ${computerSelection} beats ${playerSelection}.`);
+        roundInfo.textContent = `You Lose! ${computerSelection} beats ${playerSelection}.`;
     }
 
     if (computerSelection === "scissors" && playerSelection === "scissors") {
-        window.alert("It`s a draw!");
+        roundInfo.textContent = "It's a draw!";
     }
 
     //prints the player`s and computer`s score log
-    window.alert(`Score is: You ${playerScore} x Computer ${computerScore}`);
+    if (playerScore === 5) {
+        score.textContent = `You win!!! Final score is: You ${playerScore} x Computer ${computerScore}`;
+    } else if (computerScore === 5) {
+        score.textContent = `You lose!!! Final score is: You ${playerScore} x Computer ${computerScore}`;
+    } else {
+        score.textContent = `Score is: You ${playerScore} x Computer ${computerScore}`;
+    }
 }
 
 
-playRound(playerSelection, computerSelection);
-playRound(playerSelection, computerSelection);
-playRound(playerSelection, computerSelection);
-playRound(playerSelection, computerSelection);
-playRound(playerSelection, computerSelection);
+
+
+const rock = document.getElementById('rock');
+rock.addEventListener('click', function(e) {
+    let playerPlay = 'rock';
+    playRound(playerPlay, computerPlay);
+});
+
+const paper = document.getElementById('paper');
+paper.addEventListener('click', function(e) {
+    let playerPlay = 'paper';
+    playRound(playerPlay, computerPlay);
+});
+
+const scissors = document.getElementById('scissors');
+scissors.addEventListener('click', function(e) {
+    let playerPlay = 'scissors';
+    playRound(playerPlay, computerPlay);
+});
+
+const roundInfo = document.querySelector('.roundInfo');
+const score = document.querySelector('.score');
